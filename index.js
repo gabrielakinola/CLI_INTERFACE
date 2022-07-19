f2c5a5cd7b684acc63b8c45479685ec31b9eaafe
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-07-19 22:02:04 +0100

    Creating a CLI with node.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Customer = require("./models/customer");
 
 //connect to db
 mongoose
@@ -7,3 +8,29 @@ mongoose
   .catch((err) => console.log(err));
 
 const db = mongoose.connection;
+
+//Add customer
+const addCustomer = (customer) => {
+  Customer.create(customer).then((customer) => {
+    console.info("New Customer Added");
+    db.close();
+  });
+};
+
+//Find customer
+const findCustomer = (name) => {
+  //Make case insensitive
+  const search = new RegExp(name, "i");
+  Customer.find({ $or: [{ firstname: search }, { lastname: search }] }).then(
+    (customer) => {
+      console.info(customer);
+      console.info(`${customer.length} matches`);
+      db.close();
+    }
+  );
+};
+
+module.exports = {
+  addCustomer,
+  findCustomer,
+};

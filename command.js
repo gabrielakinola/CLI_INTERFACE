e3051b81d70ff1b8ea9f9584f018a14533bcd5ad
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-07-21 00:00:01 +0100

    Create a command line interface using node.js

--- command.js
+++ command.js
@@ -1,2 +1,6 @@
 const program = require("commander");
 const { addCustomer, findCustomer } = require("./index");
+
+program.version("1.0.0").description("Client Management System");
+
+program.command("add <firstname> <lastname> <phone> <email> ");
